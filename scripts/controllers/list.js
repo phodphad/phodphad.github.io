dcc81db525afacd55e92d5bbd086d9202c0f2a69
Nikhil Daga <nikhil.daga.bitsian@gmail.com>
2015-05-10 20:32:45 +0530

    Update list.js

--- scripts/controllers/list.js
+++ scripts/controllers/list.js
@@ -16,8 +16,7 @@ tooglesApp.controller('ListCtrl', ['$scope', '$routeParams', '$location', 'youtu
       $scope.videos.push.apply($scope.videos, data.feed.entry);
     }
   }
-  $scope.videos.shift();
-  
+
   window.userCallback = function(data) {
     $scope.user = data.entry;
   }
