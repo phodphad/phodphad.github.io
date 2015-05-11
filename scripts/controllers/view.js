dd8f9c13a479fef1f97d1e2b3893d132a8dff9e8
Nikhil Daga <nikhil.daga.bitsian@gmail.com>
2015-05-11 17:57:33 +0530

    Update view.js

--- scripts/controllers/view.js
+++ scripts/controllers/view.js
@@ -9,59 +9,57 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
   $scope.section = $location.path().split('/')[1];
   $scope.videoTab = $scope.section === 'view' ? 'Related' : 'Playlist';
 
-  window.viewCallback = function(data) {
-    if ($scope.section === 'view') {
-      $scope.video = data.entry;
-      $scope.video.video_id = $routeParams.id;
-      $scope.video.embedurl = "http://www.youtube.com/embed/" + $scope.video.video_id + "?autoplay=1&theme=light&color=white&iv_load_policy=3&origin=http://toogl.es";
-    } else {
-      var start = $routeParams.start || 0;
-      $scope.video = data.feed.entry[start];
-      $scope.video.video_id = $scope.video.media$group.yt$videoid.$t;
-      $scope.video.embedurl = "http://www.youtube.com/embed/videoseries?list=" + $routeParams.id + "&autoplay=1&theme=light&color=white&iv_load_policy=3&origin=http://toogl.es&index=" + start;
-      $scope.videos = data.feed.entry;
+  if (localStorage.tooglesDarkMode === "true") {
+    $scope.$parent.darkmode = true;
+  }
+  $scope.$watch('darkmode', function (newVal, oldVal, scope) {
+    if (typeof newVal !== "undefined" && newVal !== "undefined") {
+      localStorage.tooglesDarkMode = newVal;
     }
-    onYouTubeIframeAPIReady($scope.video.video_id, $scope.section);
-    document.title = $scope.video.title.$t + " | Phodphad";
-  }
+  });
 
-  window.relatedCallback = function(data) {
-    $scope.videos = data.feed.entry;
-  }
-
-  $scope.fetchRelated = function() {
-    if (!$scope.videos) {
-      youtube.setCallback('relatedCallback');
-      youtube.getVideos('related', $routeParams.id);
-    }
-    $scope.showRelated = true;
-  }
-
-  $scope.getLink = function(video, index) {
-    if ($scope.section == 'view') {
-      return '#/view/' + youtube.urlToID(video.media$group.yt$videoid.$t);
-    } else if ($scope.section = 'playlist') {
-      return '#/playlist/' + $routeParams.id + '/' + index
-    }
-  }
+  youtube.fetchVideos($routeParams.id, function(response) {
+    $scope.video = response.items[0];
+    onYouTubeIframeAPIReady($scope.video.id, $scope.section);
+    document.title = $scope.video.title + " | Toogles";
+  });
 
   $scope.formatDuration = function(seconds) {
     return youtube.formatDuration(seconds);
-  }
+  };
 
-  youtube.setCallback('viewCallback');
-  if ($scope.section === 'view') {
-    youtube.getItem('videos', $routeParams.id);
+  var started = false;
+
+  if ($scope.$parent.darkmode) {
+    var theme = 'dark';
+    var color = 'black';
   } else {
-    youtube.getItem('playlists', $routeParams.id);
+    var theme = 'light';
+    var color = 'white';
   }
-
-  started = false;
 
   var onYouTubeIframeAPIReady = function(id, section) {
+    var starttime = $routeParams.starttime || 0;
+
     var player = new YT.Player('player', {
-      videoId: id,
-      events: {
+      'videoId': id,
+      'playerVars': {
+        'autoplay': 1,
+        'theme': theme,
+        'color': color,
+        'iv_load_policy': 3,
+        'origin': 'http://toogl.es',
+        'start': starttime
+      },
+      'events': {
+        'onError': function(event) {
+          if (event.data == 101 || event.data == 150) {
+            $scope.video.restricted = 1;
+          } else {
+            $scope.video.deleted = 1;
+          }
+          $scope.$apply();
+        },
         'onStateChange': function (event) {
           if (event.data == 1) {
             started = true;
@@ -70,14 +68,30 @@ tooglesApp.controller('ViewCtrl', ['$scope', '$routeParams', '$location', 'youtu
             // When a new video is started in an existing player, open up its dedicated page.
             if (section === 'view') {
               var video_url = event.target.getVideoUrl();
-              var video_id = video_url.replace('http://www.youtube.com/watch?v=', '').replace('&feature=player_embedded', '');
+              var video_id = video_url.replace('http://', '').replace('https://', '').replace('www.youtube.com/watch?v=', '').replace('&feature=player_embedded', '');
               window.location = '#/view/' + video_id;
             } else if (section === 'playlist') {
               window.location = '#/playlist/' + event.target.getPlaylistId() + '/' + event.target.getPlaylistIndex();
             }
           }
+          else if (started && event.data == 0 && section === 'playlist') {
+            var pathParts = $location.path().split('/');
+            var playlistId = pathParts[2];
+            if (pathParts[3]) {
+              var videoNum = pathParts[3];
+            } else {
+              var videoNum = 0;
+            }
+            videoNum++;
+
+            window.location = '#/playlist/' + playlistId + '/' + videoNum;
+          }
         }
       }
     });
+  }
+
+  $scope.averageRating = function(likeCount, dislikeCount) {
+    return youtube.averageRating(likeCount, dislikeCount);
   }
 }]);
