15fea9ff396005464cb256701dd8ad14d638af33
Nikhil Daga <nikhil.daga.bitsian@gmail.com>
2015-05-11 18:22:49 +0530

    phodphad v3

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,6 @@
 var tooglesApp = angular.module('tooglesApp', ['ngAnimate', 'ngAria', 'ngCookies', 'ngMessages', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/browse', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
-    $routeProvider.when('/home', { templateUrl: 'views/home.html', controller: 'ListCtrl' });    
     $routeProvider.when('/browse/:category', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
     $routeProvider.when('/search/:query', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
     $routeProvider.when('/view/:id', { templateUrl: 'views/view.html', controller: 'ViewCtrl' });
@@ -18,10 +17,5 @@ var tooglesApp = angular.module('tooglesApp', ['ngAnimate', 'ngAria', 'ngCookies
     $routeProvider.when('/playlist/:id/:start', { templateUrl: 'views/view.html', controller: 'ViewCtrl' });
     $routeProvider.when('/user/:username', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
     $routeProvider.when('/user/:username/:feed', { templateUrl: 'views/list.html', controller: 'ListCtrl' });
-<<<<<<< HEAD
     $routeProvider.otherwise({ redirectTo: '/browse' });
   }]);
-=======
-    $routeProvider.otherwise({ redirectTo: '/home' });
-  }]);
->>>>>>> f117e5b30713b78b56111f6b145fab4e2a6572b4
